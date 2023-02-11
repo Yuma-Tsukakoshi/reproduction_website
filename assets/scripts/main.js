4dfc906cde32f4cb161bcfef2604ff9b74b4c4d4
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-02-11 13:24:35 +0900

    ハンバーガーメニュー以外できた

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,2 +1,67 @@
 'use strict';
 
+let box_html1 = "";
+let box_html2 = "";
+for(let i=0 ; i<16 ; i++){
+  if(i<12){
+    box_html1 += `<div class="product-box" data_id="${i+1}">
+  <a class="image-link"><img src="./assets/img/item${i+1}.jpg" alt="product-img"></a>
+  <h3 class="product-title">プロダクトタイトルプロダクトタイトル</h3>
+  <p class="money">￥99,999+tax</p>
+</div>`
+  }else{
+    box_html2 += `<div class="product-box" data_id="${i+1}">
+  <a class="image-link"><img src="./assets/img/item${i+1}.jpg" alt="product-img"></a>
+  <h3 class="product-title">プロダクトタイトルプロダクトタイトル</h3>
+  <p class="money">￥99,999+tax</p>
+</div>`
+  }
+}
+
+$("#js-products-1").html(box_html1);
+$("#js-products-2").html(box_html2).hide();
+
+const createProductHTML=(selectNumber)=>{
+  let ProductHTML = "";
+  ProductHTML += `<div class="product-page">
+  <h2 class="praduct-title">プロダクトタイトル</h2>
+  <div class="product-inner">
+    <figure class="product-img">
+      <img src="./assets/img/item${selectNumber}.jpg" alt="">
+    </figure>
+    <div class="product-content">
+      <p class="product-introduce">テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+      <p class="product-introduce">テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+      <p class="money">￥99,999+tax</p>
+      <div class="product-detail">
+        <ul class="detail-list">
+          <li class="detail-items title">SIZE:<span class="text">W999 × D999 × H999</span></li>
+          <li class="detail-items title">COLOR:<span class="text">テキスト</span></li>
+          <li class="detail-items title">MATERIAL:<span class="text">テキストテキストテキスト</span></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <a href="./index.html" class="back-to-product">Back To Products</a>
+</div>`
+  $(".product-link").html(ProductHTML);
+  $(".products").hide();
+  $(".main-title").hide();
+}
+
+$(".product-box").each(function(){
+  $(this).on('click',function(){
+    const selectedProduct=Number($(this).attr('data_id'))
+    createProductHTML(selectedProduct);
+  })
+})
+
+$("#link1_btn").on('click',function(){
+  $("#js-products-1").show();
+  $("#js-products-2").hide();
+})
+
+$("#link2_btn").on('click',function(){
+  $("#js-products-2").show();
+  $("#js-products-1").hide();
+})

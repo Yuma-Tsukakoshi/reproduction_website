7350dcae0d0de85a6409ed5de4b4273299f88ae1
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-02-12 23:00:34 +0900

    display absolute系の指定上手くいってない

---
+++ assets/scripts/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let sneakersHTML = "";
+for(let i=0; i<9 ; i++){
+  sneakersHTML += `<div class="product-box">
+  <figure>
+    <img src="./assets/img/feature${i+1}.jpg" alt="">
+  </figure>
+  <div class="img-content">
+    <h3 class="category">category</h3>
+    <p class="content-text">テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+    <p class="content-date">XXXX.XX.XX</p>
+  </div>
+</div>`
+}
+$("#js-sneakers").html(sneakersHTML);

e2e396b15ee06d7b0aba4e778fabfd5d48a820d2
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-02-15 06:40:45 +0900

    slider create

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -20,3 +20,35 @@ $("#js-toggle-btn").on("click",function(){
   $(".mask").toggleClass("active");
   $("#side-bar").toggleClass("active");
 })
+
+$("#js-pick_up, #js-feature, #js-contact").on("click",function(){
+  $(".bar").removeClass("active");
+  $(".mask").removeClass("active");
+  $("#side-bar").removeClass("active");
+})
+
+$(function(){
+  /*=================================================
+  // カルーセル用 jQueryプラグイン「slick」
+  // マニュアル：https://kenwheeler.github.io/slick/
+  ===================================================*/
+  $('#slider').slick({
+    autoplay: true,                         // 自動再生オン
+    autoplaySpeed: 2000,                    // スライドを3秒で切り替え
+    // arrows: true,                           // 左右の矢印を表示
+    // dots: true,                             // ドット（ページ送り）を表示
+    slidesToShow: 1,                        // スライドを1枚表示（※centerModeをtrueにすると両端に2枚見切れた状態になる）
+    centerMode: true,                       // センターモード（両端が見切れた状態になる）
+    centerPadding: '18%',                   // 見切れたコンテンツの幅を18%に設定
+    // prevArrow:'<div class="prev"></div>',   // 前へ矢印のHTMLを変更する
+    // nextArrow:'<div class="next"></div>',   // 次へ矢印のHTMLを変更する
+    responsive: [              // レスポンシブの設定
+      {
+        breakpoint: 900,       // 900px以下の場合に適用
+        settings: {
+          centerMode: false    // センターモードをオフにする
+        }
+      }
+    ]
+  });
+});
